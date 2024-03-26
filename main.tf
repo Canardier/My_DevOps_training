738530964bbdf407264b0d9aa8f8c7785d11648d
daffy <CuckOrDuck@hotmail.com>
2024-03-26 21:28:15 +0100

    add define in vagrantfile

--- main.tf
+++ main.tf
@@ -11,17 +11,6 @@ provider "vagrant" {
   # no config
 }
 
-resource "vagrant_vm" "worker" {
-  vagrantfile_dir = "./worker"
-  env = {
-    name = "worker-${count.index + 1}"
-    ip_address = "192.168.10.${count.index + 11}"
-    vm_hostname = "worker-${count.index + 1}"
-  }
-  get_ports = true
-  count = 1
-}
-
 resource "vagrant_vm" "master" {
   vagrantfile_dir = "./master"
   env = {
@@ -30,4 +19,15 @@ resource "vagrant_vm" "master" {
     vm_hostname = "master"
   }
   get_ports = true
+}
+
+resource "vagrant_vm" "worker" {
+  vagrantfile_dir = "./worker"
+  env = {
+    name = "worker-${count.index + 1}"
+    ip_address = "192.168.10.${count.index + 11}"
+    vm_hostname = "worker-${count.index + 1}"
+  }
+  get_ports = true
+  count = 2
 }
