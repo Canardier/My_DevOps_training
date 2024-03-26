b819c341da838bafdfd2c5f5cae296b9362cc638
daffy <CuckOrDuck@hotmail.com>
2024-03-26 19:12:53 +0100

    add project

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to launch program
+
+## Terraform
+```
+terraform init
+terraform plan -out binary_file #create binary of plan
+terraform apply "binary_file" #apply by binary
+```
---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    vagrant = {
+      source  = "bmatcuk/vagrant"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "vagrant" {
+  # no config
+}
+
+resource "vagrant_vm" "worker" {
+  vagrantfile_dir = "./worker"
+  env = {
+    name = "worker-${count.index + 1}"
+    ip_address = "192.168.10.${count.index + 11}"
+    vm_hostname = "worker-${count.index + 1}"
+  }
+  get_ports = true
+  count = 1
+}
+
+resource "vagrant_vm" "master" {
+  vagrantfile_dir = "./master"
+  env = {
+    name = "master"
+    ip_address = "192.168.10.10"
+    vm_hostname = "master"
+  }
+  get_ports = true
+}
