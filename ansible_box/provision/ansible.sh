420d80d9213c9aca0350c29b11427ae78d2472b6
studer_j <studer_j@etna-alternance.net>
2024-06-12 12:39:04 +0200

    modif box of vagrantfile from ubuntu 12.0 to 24.0 + try playbook for install, config and join kubernetes server

--- ansible_box/provision/ansible.sh
+++ ansible_box/provision/ansible.sh
@@ -3,5 +3,4 @@
 sudo sed -i '/PasswordAuthentication no/c\PasswordAuthentication yes' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 sudo apt update -y
-sudo apt install ansible -y
 sudo cat ~/inventory.ini
